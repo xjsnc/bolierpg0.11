120480037f155f0a9c21bf584a52b6f84cfbeee8
Poley Kid <juicetin.kim@gmail.com>
2024-04-24 19:56:12 -0400

    fixed

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,52 +3,69 @@ extends CharacterBody2D
 const speed = 100
 var current_dir = "none"
 
+func _start():
+	$AnimatedSprite2D.play("front_idle")
+#play front idle
+
 func _physics_process(delta):
 	player_movement(delta)
 	
 func player_movement(delta):
 	if Input.is_action_pressed("ui_right"):
 		current_dir = "right"
-		play_anim()
+		play_anim(1)
 		velocity.x = speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_left"):
 		current_dir = "left"
-		play_anim()
+		play_anim(1)
 		velocity.x = -speed
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_down"):
 		current_dir = "down"
-		play_anim()
+		play_anim(1)
 		velocity.x = 0
 		velocity.y = speed
 	elif Input.is_action_pressed("ui_up"):
 		current_dir = "up"
-		play_anim()
+		play_anim(1)
 		velocity.x = 0
 		velocity.y = -speed
 	else: 
+		play_anim(0)
 		velocity.x = 0
 		velocity.y = 0
 		
 	move_and_slide()
 
-func play_anim():
+func play_anim(movement):
 	var dir = current_dir
 	var anim = $AnimatedSprite2D
 	
 	if dir == "right":
 		anim.flip_h = false
-		anim.play("side_walk")
+		if movement == 0:
+			anim.play("side_idle")
+		elif movement == 1:
+			anim.play("side_walk")
 	elif dir == "left":
 		anim.flip_h = true
-		anim.play("side_walk")
+		if movement == 0:
+			anim.play("side_idle")
+		elif movement == 1:
+			anim.play("side_walk")
 	elif dir == "down":
 		anim.flip_h = true
-		anim.play("front_walk")
+		if movement == 0:
+			anim.play("front_idle")
+		elif movement == 1:
+			anim.play("front_walk")
 	elif dir == "up":
 		anim.flip_h = true
-		anim.play("back_walk")
+		if movement == 0:
+			anim.play("back_idle")
+		elif movement == 1:
+			anim.play("back_walk")
 	
 		
 #if movement = 0: play idle
